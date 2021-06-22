9bdd1f12ff017f46a72010fc8e7addb301b7f820
EmilHvitfeldt <emilhhvitfeldt@gmail.com>
2021-06-21 21:30:02 -0700

    setup all_themes

--- R/all_themes.R
+++ R/all_themes.R
@@ -1,9 +1,37 @@
-all_themes <- list(
-  tvthemes::theme_avatar,
-  tvthemes::theme_brooklyn99,
-  tvthemes::theme_parksAndRecLight,
-  tvthemes::theme_rickAndMorty,
-  tvthemes::theme_simpsons,
-  tvthemes::theme_spongeBob
-)
 
+all_themes <- list()
+
+if (requireNamespace("ggplot2", quietly = TRUE)) {
+  palettes <- c(
+    "theme_grey",
+    "theme_bw",
+    "theme_linedraw",
+    "theme_light",
+    "theme_dark",
+    "theme_minimal",
+    "theme_classic",
+    "theme_void",
+    "theme_test"
+  )
+
+  all_themes <- c(
+    all_themes,
+    lapply(palettes, getExportedValue, ns = "ggplot2")
+  )
+}
+
+if (requireNamespace("tvthemes", quietly = TRUE)) {
+  palettes <- c(
+    "theme_avatar",
+    "theme_brooklyn99",
+    "theme_parksAndRecLight",
+    "theme_rickAndMorty",
+    "theme_simpsons",
+    "theme_spongeBob"
+  )
+
+  all_themes <- c(
+    all_themes,
+    lapply(palettes, getExportedValue, ns = "tvthemes")
+  )
+}
